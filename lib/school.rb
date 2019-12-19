5db08093200b19c4f51b0d7f0fed5754d18b5669
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-19 21:49:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,12 +6,5 @@ class School
     @roster = {}
   end
 
-  def add_student(student_name, grade)
-    if @roster[grade] == true
-      @roster[grade] << student_name
-    else
-      @roster[grade] = []
-      @roster[grade] << student_name
-    end
-  end
+  
 end
